c05ac07092cd3fd4589d587fe4e1deaba896f83f
swap1408 <96335058+swap1408@users.noreply.github.com>
2025-07-22 17:15:02 +0530

    Update index.js
    Signed-off-by: swap1408 <96335058+swap1408@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 3000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Swapnil !')
+  response.send('Hello Swapnil g !')
 })
 
 app.listen(app.get('port'), function() {
